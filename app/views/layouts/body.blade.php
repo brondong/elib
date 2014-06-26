4022c5218fa7732ccf8067b2aeb2444fa8f59e59
Heru Rusdianto <cimay_hideto@yahoo.com>
2014-06-26 22:39:59 +0700

    tambah navbar, menu & konten

--- app/views/layouts/body.blade.php
+++ app/views/layouts/body.blade.php
@@ -1,9 +1,30 @@
 @include('layouts.header')
 
 <body>
-  <div class="container">
+  @include('includes.navbar')
+
+  <!-- .container -->
+  <div class="container-fluid">
+
+    <!-- .row -->
+    <div class="row">
+
+      <!-- #menu -->
+      <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3" id="menu">
+        @include('includes.menu')
+      </div>
+      <!-- /#menu -->
+
+      <!-- #konten -->
+      <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9" id="konten">
+        @yield('konten')
+      </div>
+      <!-- /#konten -->
+
+    </div>
+    <!-- /.row -->
 
   </div>
-  <!-- /.container -->
+  <!-- .container -->
 
 @include('layouts.footer')
