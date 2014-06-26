4aacc4912d7bc2dc2af9afaec2ed531b6d9ea369
Heru Rusdianto <cimay_hideto@yahoo.com>
2014-06-26 20:58:36 +0700

    buat file ValidasiLogin.php

---
+++ app/Elib/Validasi/ValidasiLogin.php
@@ -0,0 +1,15 @@
+<?php namespace Elib\Validasi;
+
+class ValidasiLogin extends Validasi {
+
+  /**
+   * rules validasi login
+   * 
+   * @var array
+   */
+  protected static $rules = array(
+    'email'    => 'required|max:50|email',
+    'password' => 'required'
+  );
+
+}
