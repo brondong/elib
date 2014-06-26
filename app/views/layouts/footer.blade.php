74940c55d3acd3be812384cd18157de57b8c2fb7
Heru Rusdianto <cimay_hideto@yahoo.com>
2014-06-26 22:46:30 +0700

    tambah load script.js

--- app/views/layouts/footer.blade.php
+++ app/views/layouts/footer.blade.php
@@ -1,6 +1,7 @@
   <!-- javascript -->
   <script src="{{ asset('js/jquery.min.js') }}"></script>
   <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('js/script.js') }}"></script>
   <!-- /javascript -->
 
   @yield('js')
