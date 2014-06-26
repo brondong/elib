757b9bb455e563ae86a782911f7b8ae78c30e42f
Heru Rusdianto <cimay_hideto@yahoo.com>
2014-06-27 00:14:05 +0700

    buat seeder tabel aktivitas

---
+++ app/database/seeds/SeederTabelAktivitas.php
@@ -0,0 +1,28 @@
+<?php
+class SeederTabelAktivitas extends Seeder {
+
+  public function run()
+  {
+    // Aktivitas::truncate();
+
+    $date = new DateTime();
+
+    $aktivitas = array(
+      array(
+        'tipe'       => 'login',
+        'keterangan' => 'Login ke dalam aplikasi.',
+        'created_at' => $date->format('Y-m-d H:i:s'),
+        'updated_at' => $date->format('Y-m-d H:i:s')
+      ),
+      array(
+        'tipe'       => 'logout',
+        'keterangan' => 'Logout dari aplikasi.',
+        'created_at' => $date->format('Y-m-d H:i:s'),
+        'updated_at' => $date->format('Y-m-d H:i:s')
+      )
+    );
+
+    Aktivitas::insert($aktivitas);
+  }
+
+}
