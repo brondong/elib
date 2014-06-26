c9f0816df78411021ae2f0f9bc24d9e65cdedded
Heru Rusdianto <cimay_hideto@yahoo.com>
2014-06-26 21:56:38 +0700

    buat seeder tabel user

---
+++ app/database/seeds/SeederTabelUsers.php
@@ -0,0 +1,25 @@
+<?php
+class SeederTabelUsers extends Seeder {
+
+  public function run()
+  {
+    User::truncate();
+
+    $date = new DateTime();
+
+    $user = array(
+      array(
+        'foto'       => null,
+        'nama'       => 'Admin',
+        'telp'       => '0123456789',
+        'email'      => 'admin@local.com',
+        'password'   => Hash::make('adminlocal'),
+        'created_at' => $date->format('Y-m-d H:i:s'),
+        'updated_at' => $date->format('Y-m-d H:i:s')
+      )
+    );
+
+    User::insert($user);
+  }
+
+}
