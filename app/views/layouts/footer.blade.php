455abd10118148a82f032043f124b21cdec71f63
Heru Rusdianto <cimay_hideto@yahoo.com>
2014-06-26 22:07:40 +0700

    buat layout footer

---
+++ app/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+  <!-- javascript -->
+  <script src="{{ asset('js/jquery.min.js') }}"></script>
+  <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+  <!-- /javascript -->
+
+  @yield('js')
+
+</body>
+</html>
