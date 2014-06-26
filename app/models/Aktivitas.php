18b572c0329290d9c0f11effde7d379ddcf3e522
Heru Rusdianto <cimay_hideto@yahoo.com>
2014-06-27 00:15:01 +0700

    buat model aktivitas

---
+++ app/models/Aktivitas.php
@@ -0,0 +1,19 @@
+<?php
+
+class Aktivitas extends Eloquent {
+
+  /**
+   * The database table used by the model.
+   *
+   * @var string
+   */
+  protected $table = 'aktivitas';
+
+  /**
+   * The attributes excluded from the model's JSON form.
+   *
+   * @var array
+   */
+  protected $hidden = array();
+
+}
