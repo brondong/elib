bd17251678ba744a3c8f3f336b0a02af6b574671
Heru Rusdianto <cimay_hideto@yahoo.com>
2014-06-26 22:42:24 +0700

    buat navbar

---
+++ app/views/includes/navbar.blade.php
@@ -0,0 +1,11 @@
+<!-- navbar  -->
+<nav class="navbar navbar-elib" role="navigation">
+
+  <div class="navbar-header">
+    <span class="navbar-brand">
+      Elib<small>1.0</small>
+    </span>
+  </div>
+
+</nav>
+<!-- /.navbar  -->
